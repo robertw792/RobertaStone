6bc34cf7a49d5b368228d3bfbc0b6a4daa35a4b9
davidbowie3579 <robert.williams1994@gmail.com>
2022-07-04 14:09:03 +0100

    installed automapper and added row class to a div tag

--- RobertaStone/Global.asax.cs
+++ RobertaStone/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using RobertaStone.App_Start;
 
 namespace RobertaStone
 {
@@ -8,6 +11,8 @@ namespace RobertaStone
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
